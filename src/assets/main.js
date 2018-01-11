3b83496ebceadbc4bb2112b6fea660b6ea3e38fd
Or seidman <or.seidman@fiverr.com>
2018-01-11 20:54:10 +0200

    fix

--- src/assets/main.js
+++ src/assets/main.js
@@ -14,28 +14,26 @@ function guess() {
         attempt.value++;
     }
 
-    if(getResults(input)) {
+     if(getResults(input.value)) {
         setMessage('You Win! :)');
         showAnswer(true);
         showReplay();
-    } else if (attempt >= 10) {
-        setMessage("You lose :(");
-        showAnswer(true);
+    } else if(attempt.value >= 10) {
+        setMessage('You Lose! :(');
+        showAnswer(false);
         showReplay();
     } else {
-        setMessage("Incorrect, try again.");
+        setMessage('Incorrect, try again.');
     }
 }
 
 
 function setHiddenFields() {
-  answer.value = Math.floor(Math.random * 10000).toString();
-
-  while(answer.value.length < 4) {
-    answer.value = "0" + answer.value;
-  }
-
-  attempt.value = 0;
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while(answer.value.length < 4) {
+        answer.value = "0" + answer.value;
+    }
+    attempt.value = 0;
 }
 
 function setMessage(label) {
@@ -51,30 +49,30 @@ function validateInput(input) {
   return true;
 }
 
-function getResults(input) {
-  let html = '<div class="row"><span class="col-md-6">' + input + '</span><span class="col-md-6">';
-  let correct = 0;
-
-  for(i=0; i< input.length; i++) {
-    if(input.charAt(i) == answer.value.charAt(i)) {
-      html += '<span class="glyphicon glyphicon-ok"></span>';
-      correct++;
-    } else if (answer.value.indexOf(input.charAt(i) > -1)) {
-        html += '<span class="glyphicon glyphicon-transfer"></span>';
-    } else{
-        html += '<span class="glyphicon glyphicon-remove"></span>';
+function getResults(input){
+    let correct = 0;
+    let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for(i = 0; i < input.length; i++)
+    {
+        if(input.charAt(i) == answer.value.charAt(i))
+        {
+            html += '<span class="glyphicon glyphicon-ok"></span>';
+            correct++;
+        } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+            html += '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            html += '<span class="glyphicon glyphicon-remove"></span>';
+        }
     }
-  }
+    html += '</div></div>';
 
-  html += '</div></div>';
+    document.getElementById('results').innerHTML += html;
 
-  document.getElementById('results').innerHTML += html;
-
-  if(correct == input.length) {
-    return true;
-  } else {
-    return false;
-  }
+    if(correct == input.length) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function showAnswer(success) {
